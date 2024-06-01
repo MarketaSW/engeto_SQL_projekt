9150606f2ee095fd06029f925293a568d365cada
MarketaSW <marketa.wallo@gmail.com>
2024-06-01 10:39:09 +0200

    Add header

---
+++ SQL_project.sql
@@ -0,0 +1,27 @@
+/*
+SQL_project.sql: první projekt do Engeto Online Data Akademie
+author: Markéta Svěráková Wallo
+email: marketa.wallo@gmail.com
+discord: marketasverakova_37252
+*/
+
+
+/* Výstupní tabulky:
+t_{jmeno}_{prijmeni}_project_SQL_primary_final (pro data mezd a cen potravin za Českou republiku sjednocených
+na totožné porovnatelné období – společné roky) a t_{jmeno}_{prijmeni}_project_SQL_secondary_final (pro dodatečná
+data o dalších evropských státech).
+*/
+
+-- do průvodní listiny: popis mezivýsledků (průvodní listinu) a informace o výstupních datech (například kde chybí hodnoty apod.)
+
+/* Výzkumné otázky
+
+- Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
+- Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
+- Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
+- Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
+- Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce,
+projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
+*/
+
+
