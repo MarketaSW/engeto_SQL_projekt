440d10d3e597b15f5a3d9e6619442211d2730a42
MarketaSW <marketa.wallo@gmail.com>
2024-07-02 10:34:01 +0200

    Queries for questions 3 and 4

--- SQL_project.sql
+++ SQL_project.sql
@@ -45,12 +45,12 @@ FROM czechia_price cp2 ;
 
 -- 2/ ROZDÍL ROČNÍCH MEZD PRO JEDNOTLIVÁ ODVĚTVÍ:
 
--- Výběr hodnoty dle číselníku:
+-- a) Výběr hodnoty dle číselníku:
 SELECT *
 FROM czechia_payroll_value_type cpvt ;
 -- -> 5958 Průměrná hrubá mzda na zaměstnance
 
--- Průměrné mzdy po letech:
+-- b) Průměrné mzdy po letech:
 SELECT 
 	ROUND(AVG(cp.value)) AS avg_salary,
 	cp.industry_branch_code,
@@ -62,7 +62,7 @@ SELECT
 	GROUP BY industry_branch_code , payroll_year
 ;
 
--- Rozdíly po letech:
+-- c)Rozdíly po letech:
 
 WITH AvgSalaries AS (
 	SELECT 
@@ -85,7 +85,7 @@ SELECT a.industry_branch_code,
 	AND a.payroll_year = b.payroll_year + 1
 ;
    
--- Nárust vs. pokles:
+-- d) Nárust vs. pokles:
 
    WITH AvgSalaries AS (
     SELECT 
@@ -135,9 +135,9 @@ ORDER BY
 
 -- Ve všech odvětvích mzdy rostly.
 
--- 3/ - Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
+-- 3/ - KOLIK JE MOŽNÉ SI KOUPIT LITRŮ MLÉKA A KILOGRAMŮ CHLEBA ZA PRVNÍ A POSLEDNÍ SROVNATELNÉ OBDOBÍ V DOSTUPNÝCH DATECH CEN A MEZD?
    
--- Průměrná roční mzda za rok 2006 a 2018 po odvětvích
+-- a) Průměrná roční mzda za rok 2006 a 2018 po odvětvích
    
    SELECT 
     cp.industry_branch_code,
@@ -152,7 +152,7 @@ WHERE
 GROUP BY 
     cp.industry_branch_code;
 
--- Průměrná roční cena za litr mléka a kg chleba v roce 2006 a 2018
+-- b) Průměrná roční cena za litr mléka a kg chleba v roce 2006 a 2018
    
    SELECT *
    FROM czechia_price_category cpc 
@@ -174,7 +174,7 @@ FROM czechia_price cp
 ;
 
 
--- mzda/cena za potravinu = jednotek
+-- c) mzda/cena za potravinu = jednotek
 WITH AvgSalaries AS (
     SELECT 
         cp.industry_branch_code,
@@ -220,3 +220,187 @@ FROM
     AvgPrices p    
 ORDER BY 
     s.industry_branch_code;
+   
+-- 4/ KTERÁ KATEGORIE POTRAVIN ZDRAŽUJE NEJPOMALEJI (je u ní nejnižší percentuální meziroční nárůst)? 
+
+-- a) průměrná roční cena u jednotlivých kategorií:
+   
+   SELECT 
+	ROUND(AVG(cpr.value), 2) AS avg_price,
+	cpr.category_code ,
+	cpr.date_from 	
+	FROM czechia_price cpr
+	WHERE YEAR(cpr.date_from) BETWEEN 2006 AND 2018 
+	AND YEAR(cpr.date_to) BETWEEN 2006 AND 2018
+	GROUP BY cpr.category_code, YEAR(cpr.date_from) ;
+
+-- b) meziroční změna:
+   
+WITH AvgPrices AS (
+    SELECT 
+        ROUND(AVG(cpr.value), 2) AS avg_price,
+        cpr.category_code,
+        YEAR(cpr.date_from) AS `year`
+    FROM 
+        czechia_price cpr
+    WHERE 
+        YEAR(cpr.date_from) BETWEEN 2006 AND 2018
+    GROUP BY 
+        cpr.category_code, YEAR(cpr.date_from)
+)
+SELECT 
+    a.category_code,
+    a.`year`,
+    a.avg_price AS current_price,
+    b.avg_price AS previous_price,
+    (a.avg_price - b.avg_price) AS yearly_diff,
+    ROUND(((a.avg_price - b.avg_price) / b.avg_price) * 100, 2) AS yearly_diff_pct
+FROM 
+    AvgPrices a
+LEFT JOIN 
+    AvgPrices b ON a.category_code = b.category_code 
+    AND a.`year` = b.`year` + 1
+ORDER BY 
+    a.category_code, a.`year`;
+   
+   -- c) výše meziročního procentuálního nárustu
+      
+WITH AvgPrices AS (
+    SELECT 
+        ROUND(AVG(cpr.value), 2) AS avg_price,
+        cpr.category_code,
+        YEAR(cpr.date_from) AS `year`
+    FROM 
+        czechia_price cpr
+    WHERE 
+        YEAR(cpr.date_from) BETWEEN 2006 AND 2018
+    GROUP BY 
+        cpr.category_code, YEAR(cpr.date_from)
+),
+YearlyDiff AS ( 
+	SELECT 
+    	a.category_code,
+    	a.`year`,
+    	a.avg_price AS current_price,
+    	b.avg_price AS previous_price,
+    	(a.avg_price - b.avg_price) AS yearly_diff,
+    	ROUND(((a.avg_price - b.avg_price) / b.avg_price) * 100, 2) AS yearly_diff_pct
+	FROM 
+    	AvgPrices a
+	LEFT JOIN 
+    	AvgPrices b ON a.category_code = b.category_code 
+    	AND a.`year` = b.`year` + 1
+	ORDER BY 
+    	a.category_code, a.`year`)
+SELECT 
+	category_code,
+	cpc.name,
+	ROUND(AVG(yearly_diff_pct), 2) AS avg_annual_increase
+    FROM 
+        YearlyDiff
+    JOIN czechia_price_category cpc ON category_code = cpc.code     
+    GROUP BY 
+        category_code, cpc.name
+    ORDER BY
+   		avg_annual_increase
+   	;
+   
+-- -> Nejpomaleji zdražuje krystalový cukr.
+
+/* 5) EXISTUJE ROK, VE KTERÉM BYL MEZIROČNÍ NÁRUST CEN POTRAVIN VÝRAZNĚ VYŠŠÍ NEŽ RŮST MEZD
+ * (větší než 10 %)? */
+
+-- a) meziroční nárust cen potravin po kategoriích:
+   
+     
+WITH AvgPrices AS (
+    SELECT 
+        ROUND(AVG(cpr.value), 2) AS avg_price,
+        YEAR(cpr.date_from) AS `year`
+    FROM 
+        czechia_price cpr
+    WHERE 
+        YEAR(cpr.date_from) BETWEEN 2006 AND 2018
+    GROUP BY 
+    	YEAR(cpr.date_from)
+    	)
+SELECT 
+    a.`year`,
+   	ROUND(((a.avg_price - b.avg_price) / b.avg_price) * 100, 2) AS yearly_price_diff_pct
+FROM 
+    AvgPrices a
+LEFT JOIN 
+    AvgPrices b ON a.`year` = b.`year` + 1
+;
+   
+--   b) meziroční nárust mezd:
+   
+   WITH AvgSalaries AS (
+	SELECT 
+	ROUND(AVG(cp.value)) AS avg_salary,
+	cp.payroll_year 	
+	FROM czechia_payroll cp 
+	WHERE cp.value_type_code = '5958' 
+	AND cp.payroll_year BETWEEN 2006 AND 2018 
+	GROUP BY payroll_year
+	)
+SELECT
+	a.payroll_year,
+	ROUND(((a.avg_salary - b.avg_salary) / b.avg_salary) * 100, 2) AS yearly_payroll_diff_pct
+	FROM AvgSalaries a
+	LEFT JOIN AvgSalaries b ON a.payroll_year = b.payroll_year + 1
+;
+
+-- c) porovnání procentuálního růstu cen potravin a mezd:
+
+WITH AvgPrices AS (
+    SELECT 
+        ROUND(AVG(cpr.value), 2) AS avg_price,
+        YEAR(cpr.date_from) AS `year`
+    FROM 
+        czechia_price cpr
+    WHERE 
+        YEAR(cpr.date_from) BETWEEN 2006 AND 2018
+    GROUP BY 
+    	YEAR(cpr.date_from)
+    ),
+AvgPriceDiff AS (
+   	SELECT 
+    	a.`year`,
+   		ROUND(((a.avg_price - b.avg_price) / b.avg_price) * 100, 2) AS yearly_price_diff_pct
+	FROM 
+    	AvgPrices a
+	LEFT JOIN 
+    	AvgPrices b ON a.`year` = b.`year` + 1	
+   ),
+AvgSalaries AS (
+	SELECT 
+		ROUND(AVG(cp.value)) AS avg_salary,
+		cp.payroll_year 	
+	FROM czechia_payroll cp 
+	WHERE cp.value_type_code = '5958' 
+	AND cp.payroll_year BETWEEN 2006 AND 2018 
+	GROUP BY payroll_year
+	),
+AvgSalaryDiff AS (
+	SELECT
+		a.payroll_year,
+		ROUND(((a.avg_salary - b.avg_salary) / b.avg_salary) * 100, 2) AS yearly_payroll_diff_pct
+	FROM AvgSalaries a
+	LEFT JOIN AvgSalaries b ON a.payroll_year = b.payroll_year + 1
+	)
+SELECT 
+	p.`year`,
+	p.yearly_price_diff_pct,
+	s.yearly_payroll_diff_pct,
+	(p.yearly_price_diff_pct - s.yearly_payroll_diff_pct) AS yearly_difference,
+	CASE WHEN (p.yearly_price_diff_pct - s.yearly_payroll_diff_pct) > 10 THEN 1 ELSE 0 END AS flag_10_pct
+FROM AvgPriceDiff p
+JOIN AvgSalaryDiff s ON p.`year` = s.payroll_year
+WHERE p.yearly_price_diff_pct IS NOT NULL
+ORDER BY yearly_difference DESC
+;
+
+-- Takový rok neexistuje, nejblíž je tomu rok 2013, kde nárust cen byl vyšší o 7%.
+
+   
