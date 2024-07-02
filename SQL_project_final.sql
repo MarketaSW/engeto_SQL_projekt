736bdfcd88dff09007e0853d9888b7276d24b055
MarketaSW <marketa.wallo@gmail.com>
2024-07-02 16:20:16 +0200

    create_primary_table

---
+++ SQL_project_final.sql
@@ -0,0 +1,9 @@
+CREATE TABLE t_marketa_sverakova_project_SQL_primary_final (
+    year INT,
+    industry_branch_code VARCHAR(10),
+    industry_name VARCHAR(255),
+    avg_salary DECIMAL(10, 2),
+    avg_price_bread DECIMAL(10, 2),
+    avg_price_milk DECIMAL(10, 2),
+    GDP DECIMAL(15, 2)
+);
