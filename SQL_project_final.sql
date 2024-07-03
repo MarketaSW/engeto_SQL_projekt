da41b39e7ed6e64fb852041ee95967fc2149c487
MarketaSW <marketa.wallo@gmail.com>
2024-07-03 15:19:27 +0200

    Query for question 5

--- SQL_project_final.sql
+++ SQL_project_final.sql
@@ -212,3 +212,48 @@ JOIN AvgSalaryDiff s ON p.`year` = s.`year`
 GROUP BY p.`year`
 ;
 -- -> V roce 2011 narostly ceny potravin o více než 10% oproti mzdám.
+
+/* 5/ MÁ VÝŠKA HDP VLIV NA ZMĚNY VE MZDÁCH A CENÁCH POTRAVIN?
+ * Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin
+ * či mzdách ve stejném nebo následujícím roce výraznějším růstem?   */
+
+WITH 
+YearlyData AS (
+    SELECT 
+        a.`year`,
+        ROUND(((a.GDP - b.GDP) / b.GDP) * 100, 2) AS GDP_diff_pct,
+        ROUND((a.avg_price_bread + a.avg_price_milk) / 2, 2) AS avg_price,
+        ROUND((b.avg_price_bread + b.avg_price_milk) / 2, 2) AS prev_avg_price,
+        ROUND(((a.avg_salary - b.avg_salary) / b.avg_salary) * 100, 2) AS payroll_diff_pct
+    FROM 
+        t_marketa_sverakova_project_SQL_primary_final a
+    LEFT JOIN 
+        t_marketa_sverakova_project_SQL_primary_final b 
+    ON 
+        a.`year` = b.`year` + 1
+),
+Flags AS (
+    SELECT 
+        yd.`year`,
+        CASE WHEN ABS(yd.GDP_diff_pct) > 3 THEN 1 ELSE 0 END AS flag_GDP_change,
+        CASE WHEN ABS(ROUND((yd.avg_price - yd.prev_avg_price) / yd.prev_avg_price * 100, 2)) > 5 THEN 1 ELSE 0 END AS flag_price_5_pct,
+        CASE WHEN ABS(yd.payroll_diff_pct) > 5 THEN 1 ELSE 0 END AS flag_salary_5_pct
+    FROM 
+        YearlyData yd
+)
+SELECT 
+    f.`year`,
+    f.flag_GDP_change,
+    CASE WHEN f.flag_GDP_change = 1 AND f.flag_price_5_pct = 1 THEN 1 ELSE 0 END AS impact_price_1Y,
+    CASE WHEN f.flag_GDP_change = 1 AND f2.flag_price_5_pct = 1 THEN 1 ELSE 0 END AS impact_price_2Y,
+    CASE WHEN f.flag_GDP_change = 1 AND f.flag_salary_5_pct = 1 THEN 1 ELSE 0 END AS impact_salary_1Y,
+    CASE WHEN f.flag_GDP_change = 1 AND f2.flag_salary_5_pct = 1 THEN 1 ELSE 0 END AS impact_salary_2Y
+FROM 
+    Flags f
+JOIN 
+    Flags f2 ON f.`year` = f2.`year` + 1
+GROUP BY 
+    f.`year`;
+/* -> S jistotou lze říci, že pokud nedojde ke změně v HDP nad 3%, nedojde ani ke změně v cenách a mzdách o více jak 5%. 
+ * Naopak pokud se HDP zvýší o více jak 3%, lze v následujícím roce nebo dvou sledovat i proměnu v cenách a mzdách o více jak 5%.
+ */
